d3e79feb00488a50c3d19ce2ec3b51dbc7ed3591
aastikdas <aastikdas126@gmail.com>
2026-06-14 12:38:35 +0530

    app.py

--- app.py
+++ app.py
@@ -77,4 +77,9 @@ def serve_export(filename):
     return send_from_directory("exports", filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=True
+    )
